af2bf2ed2fdbb4a391ebbe3d210e28b5009a15ce
Dirk Boonzajer on Thinkpad <dbs660@vu.nl>
2015-09-04 17:08:03 +0200

    Cleaned up the code

--- team/ExtremelyHungryPlayer.py
+++ team/ExtremelyHungryPlayer.py
@@ -23,31 +23,30 @@ class ExtremelyHungryPlayer(AbstractPlayer):
 #            a.append(self.adjacency.a_star(
 #            
 #
-    def compute_adjacency_list(self):
+    def compute_food_score(self, distance_decay=1.5):
         """ Compute: distance to every pill, first step for every pill. 
             Out: dict of step options weighed by distance """
          #initialise a dict of step options: {next_cell: weight_count}
-        self.step_options = defaultdict(float)
 
         # loop through the list of available pills
         for p in self.enemy_food:
             # compute the path to the next one
             path_to_pill = self.adjacency.a_star(self.current_pos, p)
             # compute the length for scaling
-            #weight = 1./len(path_to_pill)
-            #weight = 1./len(path_to_pill)**2
-            weight = np.exp(-len(path_to_pill)/1.5)
+            weight = np.exp(-len(path_to_pill)/distance_decay)
 
             # populate the step options dict
             self.step_options[path_to_pill[-1]]+=weight
-            #recommend the step with ??A
 
-        
+    
+
+    def compute_optimal_move(self):
+        """ Compute the optimal move based on the coordinate with the highest
+        score """
+        # recommend the step with the highest score
         recommended_coordinate = max(self.step_options, key=self.step_options.get)
         self.move = diff_pos(self.current_pos, recommended_coordinate)
-        print(recommended_coordinate)
             
-
     def get_move(self):
         # check, if food is still present
         self.say("Hungry!")
@@ -56,28 +55,17 @@ class ExtremelyHungryPlayer(AbstractPlayer):
             if not self.enemy_food:
                 # all food has been eaten? ok. i’ll stop
                 return datamodel.stop
-
+        
 
             self.next_food = self.rnd.choice(self.enemy_food)
 
-        self.compute_adjacency_list()#get_adjacency_list()
-        # now, scale their importance with the distance
-        #self.next_food_importance_list =
-        #    np.exp(-next_food_distance_list/self.decay_distance)
 
-        # 
+        self.step_options = defaultdict(float)
+        self.compute_food_score()
+        self.compute_optimal_move()
 
-###        minimum_index = np.argmin(self.next_food_distance_list)
-###        # get the path to the next part of liqorice
-###        # steps = self.adjacency.a_star(self.current_pos, self.enemy_food[minimum_index])
-###        # print("Distance to first pill:",  self.next_food_distance_list)
-###        self.next_food = self.enemy_food[minimum_index]
         try:
-#            next_pos = self.goto_pos(self.next_food)
-#            print(next_pos)
-#            move = diff_pos(self.current_pos, next_pos)
-#            print(move)
-            return self.move
+           return self.move
         except NoPathException:
             print("Help!")
             return datamodel.stop
